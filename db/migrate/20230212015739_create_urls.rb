88689480724bee13e144953c1af9ff00da29f42d
thomas <samoht2@Thomas-Robinson.local>
2023-02-11 19:59:39 -0600

    Will add initial `Url`, `Home` Model View and Controllers (MVC) Changes:
    * Added initial `URL` Model view and controller
    * Also added initial unit tests for the `Url` model
    * Finally, added initial home controller and view

---
+++ db/migrate/20230212015739_create_urls.rb
@@ -0,0 +1,11 @@
+class CreateUrls < ActiveRecord::Migration[7.0]
+  def change
+    create_table :urls do |t|
+      t.string :domain
+      t.string :full_url
+      t.string :short_url
+
+      t.timestamps
+    end
+  end
+end
---
+++ test/system/urls_test.rb
@@ -0,0 +1,45 @@
+require "application_system_test_case"
+
+class UrlsTest < ApplicationSystemTestCase
+  setup do
+    @url = urls(:one)
+  end
+
+  test "visiting the index" do
+    visit urls_url
+    assert_selector "h1", text: "Urls"
+  end
+
+  test "should create url" do
+    visit urls_url
+    click_on "New url"
+
+    fill_in "Domain", with: @url.domain
+    fill_in "Full url", with: @url.full_url
+    fill_in "Short url", with: @url.short_url
+    click_on "Create Url"
+
+    assert_text "Url was successfully created"
+    click_on "Back"
+  end
+
+  test "should update Url" do
+    visit url_url(@url)
+    click_on "Edit this url", match: :first
+
+    fill_in "Domain", with: @url.domain
+    fill_in "Full url", with: @url.full_url
+    fill_in "Short url", with: @url.short_url
+    click_on "Update Url"
+
+    assert_text "Url was successfully updated"
+    click_on "Back"
+  end
+
+  test "should destroy Url" do
+    visit url_url(@url)
+    click_on "Destroy this url", match: :first
+
+    assert_text "Url was successfully destroyed"
+  end
+end
